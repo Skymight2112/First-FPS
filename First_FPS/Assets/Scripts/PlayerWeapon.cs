e01ed68b828ab9b83a8b5ec86d69e989a8689e68
Skymight2112 <jayde@primic.com>
2020-03-02 15:33:17 +0200

    Fixed Shooting and cam error
    Fixed the Raycasting issue as well as disabled the weapon for non-local players

--- First_FPS/Assets/Scripts/PlayerWeapon.cs
+++ First_FPS/Assets/Scripts/PlayerWeapon.cs
@@ -11,4 +11,26 @@ public class PlayerWeapon : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
 
+    public Camera FpsCam;
+
+
+    private void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FpsCam.transform.position, FpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+
 }
